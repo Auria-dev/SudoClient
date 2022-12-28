57f2c1945753fef45fae1ae7d400749b49990194
Nitaki Nagachiro <64524466+Nitaki-dev@users.noreply.github.com>
2022-12-28 22:10:42 +0100

    forgor offset :skull:

--- src/main/java/sudo/core/font/IFont.java
+++ src/main/java/sudo/core/font/IFont.java
@@ -1,6 +1,6 @@
 package sudo.core.font;
 
 public class IFont {
-    public static GlyphPageFontRenderer CONSOLAS = GlyphPageFontRenderer.create("Inter-Light",
+    public static GlyphPageFontRenderer CONSOLAS = GlyphPageFontRenderer.create("Inter-Medium",
             20, false, false, false);
 }
