90aa71811208f4b38e57e7cf715ef67660c02b8b
Canonball90 <46059283+Canonball90@users.noreply.github.com>
2022-12-28 11:07:06 -0500

    Gui Animations

--- src/main/java/sudo/ui/screens/clickgui/ModuleButton.java
+++ src/main/java/sudo/ui/screens/clickgui/ModuleButton.java
@@ -1,12 +1,19 @@
 package sudo.ui.screens.clickgui;
 
+import me.surge.animation.ColourAnimation;
+import me.surge.animation.Easing;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.util.math.MatrixStack;
 import sudo.module.Mod;
 
+import java.awt.*;
+
 public class ModuleButton {
 	private static MinecraftClient mc = MinecraftClient.getInstance();
+	private ColourAnimation hoverAnim = new ColourAnimation(new Color(0xff2A2A2A), new Color(0xff111111), 300F, false, Easing.LINEAR);
+	private ColourAnimation enabledAnim = new ColourAnimation(new Color(0xff545454), new Color(0xff9D73E6), 300F, false, Easing.LINEAR);
+	private ColourAnimation textEnabledAnim = new ColourAnimation(Color.WHITE, new Color(0xff9D73E6), 300F, false, Easing.LINEAR);
 
 	public Mod module;
 	public Frame parent;
@@ -19,15 +26,18 @@ public class ModuleButton {
 	}
 	
 	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+		hoverAnim.setState(isHovered(mouseX,mouseY));
+		enabledAnim.setState(module.isEnabled());
+		textEnabledAnim.setState(module.isEnabled());
 		DrawableHelper.fill(matrices, parent.x, parent.y + offset, parent.x + parent.width, parent.y+offset+parent.height, 0xff2A2A2A);
-		if(isHovered(mouseX, mouseY)) DrawableHelper.fill(matrices, parent.x, parent.y + offset, parent.x + parent.width, parent.y+offset+parent.height, 0xff111111);
+		DrawableHelper.fill(matrices, parent.x, parent.y + offset, parent.x + parent.width, parent.y+offset+parent.height, hoverAnim.getColour().getRGB());
 		DrawableHelper.fill(matrices, 
 				parent.x + parent.width - 5, 
 				parent.y + offset+2, 
 				parent.x + parent.width-2, 
 				parent.y+offset+parent.height-2,
-				module.isEnabled() ? 0xff9D73E6 : 0xff545454);
-		parent.mc.textRenderer.draw(matrices, module.getName(), parent.x + 1, parent.y+(parent.height/2)-(mc.textRenderer.fontHeight/2) + offset+1, module.isEnabled() ? 0xff9D73E6 : -1);
+				enabledAnim.getColour().getRGB());
+		parent.mc.textRenderer.draw(matrices, module.getName(), parent.x + 1, parent.y+(parent.height/2)-(mc.textRenderer.fontHeight/2) + offset+1, textEnabledAnim.getColour().getRGB());
 	}
 	
 	public void mouseClicked(double mouseX, double mouseY, int button) {
