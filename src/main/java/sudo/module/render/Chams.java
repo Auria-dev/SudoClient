23c1a1b47da596fe97eb5f9319a00cc2ea035fab
Canonball90 <46059283+Canonball90@users.noreply.github.com>
2022-12-29 10:43:23 -0500

    Chams

---
+++ src/main/java/sudo/module/render/Chams.java
@@ -0,0 +1,33 @@
+package sudo.module.render;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.decoration.EndCrystalEntity;
+import net.minecraft.entity.passive.StriderEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import sudo.module.Mod;
+import sudo.module.settings.BooleanSetting;
+
+public class Chams extends Mod {
+
+    public BooleanSetting players = new BooleanSetting("Players", true);
+    public BooleanSetting crystals = new BooleanSetting("Crystals", true);
+
+    public Chams() {
+        super("Chams", "Renders players and crystals trough walls",Category.RENDER,0);
+        addSettings(players,crystals);
+        get = this;
+    }
+
+    public boolean shouldRender(Entity entity) {
+        if (!isEnabled()) return false;
+        if (entity == null) return false;
+        if (entity instanceof StriderEntity) return false;
+        if (entity instanceof EndCrystalEntity) return crystals.isEnabled();
+        if (entity instanceof PlayerEntity && entity != mc.player) return players.isEnabled();
+
+        return false;
+    }
+
+    public static Chams get;
+
+}
